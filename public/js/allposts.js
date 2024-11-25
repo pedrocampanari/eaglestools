f6dd56fa8d22a9f30ff6e220251233d51ff0ed01
Pedro Campanari <pedrocampanari09@hotmail.com>
2024-11-25 15:34:17 -0300

    Up

--- public/js/allposts.js
+++ public/js/allposts.js
@@ -1,3 +1,9 @@
+const params = new URLSearchParams(window.location.search);
+const collectUserId = params.get('id');
+document.getElementById('username').innerHTML = params.get('name')
+
+
+console.log(collectUserId)
 const containerShowAll = document.getElementById('allposts');
 
 
@@ -6,17 +12,27 @@ const showAllPosts = async () => {
         const url = '/api/tasks/concludedAll/';
         const response = await fetch (url, {method: 'GET', headers: { "Content-Type": "application/json"}});
         const data = await response.json();
-        const posts = data.map(post => {
+        const posts = data.map((post, i) => {
             console.log(post)
             return `<div class="post card p-3">
-                <h4 style="font-size: 1.2rem" class="span-roboto-condensed"><span ><img width="30px" src="../assets/img/icon/person.png"/></span> - <i>${post.ownerName}</i></h4>
-                <p>${post.description}</p>
-                </div>`});
+                        <h4 style="font-size: 1.2rem" class="span-roboto-condensed"><span ><img width="30px" src="../assets/img/icon/person.png"/></span> - <i>${post.ownerName}</i> <button class="seeTask" type="button" onclick="seePost(${i})"class="btn">Ver</button></h4>
+                        <p style="display: -webkit-box; -webkit-line-clamp: 4; overflow: hidden; -webkit-box-orient: vertical; " class="poster">${post.description}
+                        </p>
+                    </div>`});
         containerShowAll.innerHTML = posts.join('');
     } catch (err){
-        console.log(err) 
+        console.log(err);
     }
 }
+
+const seePost = (element) =>{
+    const post = document.getElementsByClassName('poster')[element];
+    post.style.transition = '1s';
+    post.style.paddingBottom = "20px";
+    post.style.overflow = 'visible';
+
+}
+
 
 
 showAllPosts();
